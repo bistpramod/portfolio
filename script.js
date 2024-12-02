447588e1f5a5ea7947c05c84759f04c89869e03a
Pramod Bist <155552124+bistpramod@users.noreply.github.com>
2024-12-02 23:35:03 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// Scroll to a specific section
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
+// for displaying the list of experience , education and skills 
+
+let tablinks = document.getElementsByClassName("links");
+let tabcontents = document.getElementsByClassName("tabContains");
+
+
+function openTab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("activeone");
     }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("tabshow");
+    }
+    event.currentTarget.classList.add("activeone");
+    document.getElementById(tabname).classList.add("tabshow");
+
+}
+
+//for the sidebar
+
+const sideMenu = document.getElementById("sideBar");
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+function closeMenu() {
+    sideMenu.style.right = "-200px";
 }
